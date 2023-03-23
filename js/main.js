96fcd9df593ccb98071dfd83f9b5ee7843d665d6
iteenfk <125781734+iteenfk@users.noreply.github.com>
2023-03-23 19:55:16 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,40 +1,42 @@
 "use strict"
 
-const slider = document.getElementById("slider");
+function add() {
+    const firstNumber = parseFloat(document.getElementById("firstNumber").value);
+    const secondNumber = parseFloat(document.getElementById("secondNumber").value);
+    const result = firstNumber + secondNumber;
+    document.getElementById("result").value = result;
+}
 
-const btn = document.getElementById("btn");
+function subtract() {
+    const firstNumber = parseFloat(document.getElementById("firstNumber").value);
+    const secondNumber = parseFloat(document.getElementById("secondNumber").value);
+    const result = firstNumber - secondNumber;
+    document.getElementById("result").value = result;
+}
 
-slider.addEventListener("input", () =>{
-    const passwardLength = document.getElementById("passward-length");
+function multiply() {
+    const firstNumber = parseFloat(document.getElementById("firstNumber").value);
+    const secondNumber = parseFloat(document.getElementById("secondNumber").value);
+    const result = firstNumber * secondNumber;
+    document.getElementById("result").value = result;
+}
 
-    passwardLength.textContent = slider.value;
-});
+function divide() {
+    const firstNumber = parseFloat(document.getElementById("firstNumber").value);
+    const secondNumber = parseFloat(document.getElementById("secondNumber").value);
+    const result = firstNumber / secondNumber;
+    document.getElementById("result").value = result;
+}
 
-btn.addEventListener("click",() =>{
-    const result = document.getElementById("result");
-    const numbersCheckbox = document.getElementById("numbers-checkbox");
-    const symblesCheckbox = document.getElementById("symbols-checkbox");
+function clearResult() {
+    document.getElementById("firstNumber").value = 0;
+    document.getElementById("secondNumber").value = 0;
+    document.getElementById("result").value = 0;
+}
 
-    const letters = "abcdefghilkjmnopqrstuvwxtz";
-    const numbers = "0123456789";
-    const symboles =  "#$%&()+/_?!";
-
-    let passward = "";
-    let seed = letters + letters.toUpperCase();
-
-
-    if (numbersCheckbox.checked === true){
-        seed += numbers;
-    }
-
-    if(symblesCheckbox.checked === true){
-        seed += symboles;
-    }
-
-    for(let i = 0; i < slider.value; i++){
-        passward += seed[Math.floor(Math.random() * seed.length)];
-    }
-    result.textContent = passward;
-
-     
-});
+function remainder() {
+    const firstNumber = parseFloat(document.getElementById("firstNumber").value);
+    const secondNumber = parseFloat(document.getElementById("secondNumber").value);
+    const result = firstNumber % secondNumber;
+    document.getElementById("result").value = result;
+}
