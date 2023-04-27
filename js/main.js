006267cc839ec24b993835b231cf74fa32cefc3f
iteenfk <125781734+iteenfk@users.noreply.github.com>
2023-04-27 19:03:56 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,53 +1,45 @@
 "use strict"
 
-let seconds = 0;
+const api_key = "50d4c65fa058ccffec4d18694c037a13";
 
-function convertTime() {
-    let time = document.getElementById("time").value;
-    console.log(time);
-    let unit = document.getElementById("unit").value;
-    console.log(unit);
-    if (unit === "秒") {
-        seconds = time;
-    } else if (unit === "分") {
-        seconds = time * 60;
-    } else if (unit === "時間") {
-        seconds = time * 60 * 60;
-    } else if (unit === "日") {
-        seconds = time * 60 * 60 * 24;
-    } else if (unit === "週") {
-        seconds = time * 60 * 60 * 24 * 7;
-    } else if (unit === "月") {
-        seconds = time * 60 * 60 * 24 * 30;
-    } else if (unit === "年") {
-        seconds = time * 60 * 60 * 24 * 365;
-    }
+const select = document.getElementById("select");
 
-    const minutes = seconds / 60;
-    console.log(minutes);
-    const hours = minutes / 60;
-    const days = hours / 24;
-    const weeks = days / 7;
-    const months = days / 30;
-    const years = days / 365;
+console.log(select)
 
-    const secondsHtml = Number(seconds).toFixed(2) ;
-    const minutesHtml = Number(minutes).toFixed(2) ;
-    const hoursHtml = Number(hours).toFixed(2) ;
-    const daysHtml = Number(days).toFixed(2) ;
-    const weeksHtml = Number(weeks).toFixed(2) ;
-    const monthsHtml = Number(months).toFixed(2) ;
-    const yearsHtml = Number(years).toFixed(2) ;
+select.addEventListener("change",() => {
+    const city = select.value;
+    console.log(city);
+    const url =`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}&units=metric`;
 
-    const change = document.getElementById("change-box");
-    change.innerHTML = `
-    <p>変換結果を表示</p>
-    <p>${secondsHtml} 秒</p>
-    <p>${minutesHtml} 分</p>
-    <p>${hoursHtml} 時間</p>
-    <p>${daysHtml} 日</p>
-    <p>${weeksHtml} 週</p>
-    <p>${monthsHtml} 月</p>
-    <p>${yearsHtml} 年</p>
-    `;
- }
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+
+        const a = data.weather[0].main;
+        const b = data.main.temp;
+        const c = data.main.humidity;
+        const d = data.wind.speed;
+        const e = data.wind.deg;
+        const f = data.main.pressure;
+        const g = data.main.temp_max;
+        const h = data.main.temp_min;
+        const i = new Date(data.sys.sunrise * 1000).toLocaleTimeString();
+        const j = new Date(data.sys.sunset * 1000).toLocaleTimeString();
+
+        const Box = document.getElementById("box");
+        Box.innerHTML = `
+          <h3>${city}の天気情報<h3>
+          <p>天気: ${a}<p>
+          <p>気温: ${b}℃<p>
+          <p>湿度: ${c}<p>
+          <p>風速: ${d}<p>
+          <p>風向: ${e}<p>
+          <p>気圧: ${f}<p>
+          <p>最高気温: ${g}℃<p>
+          <p>最低気温: ${h}℃<p>
+          <p>日の出時刻: ${i}<p>
+          <p>日没時刻: ${j}<p>
+        `;
+    })
+    .catch(error => console.error(error));
+});
