576e2bd86bd3abffd37fd09e2d6a65d76c978fb2
iteenfk <125781734+iteenfk@users.noreply.github.com>
2023-06-08 19:43:44 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,27 +1,41 @@
 "use strict"
 
-function S() {
-    let input = document.getElementById("in"). value;
-    if (!input) {
-        alert("何も入力されていません。");
+let words = [];
+
+function A() {
+    const in1 = document.getElementById("in1");
+    const in2 = document.getElementById("in2");
+    const trim1 = in1.value.trim();
+    const trim2 = in2.value.trim();
+
+    if(!trim1 || !trim2) {
+        alert("単語と意味を入力して下さい。");
         return;
     }
 
-    let li = document.createElement("li");
-    let output = document.getElementById("out");
-    li.textContent = input;
 
-    output.appendChild(li);
+    words.push({trim1,trim2});
+    D();
+    in1.value = "";
+    in2.value = "";
+}
 
-    document.getElementById("in").value = "";
+function D() {
+    const out = document.getElementById("out");
+    out.innerHTML = "";
+    words.forEach((x) => {
+        const li = document.createElement("li");
+        li.textContent = `${x.trim1}: ${x.trim2}`;
+        out.appendChild(li);
+    });
 }
 
 function C(){
     const ul = document.getElementById("out");
     while (ul.firstChild != null) {
-        ul.removeChild(ul.firstChild)
-        console;
+        ul.removeChild(ul.firstChild);
     }
+    words = [];
 }
 
 function P(){
